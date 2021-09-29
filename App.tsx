4cc47b69f91ff7d96c088612e8cb2214352ce10f
JessicaLopesDev <jessicalopes.dev@gmail.com>
2021-09-29 10:27:36 -0300

    starting home screen, card component and global style

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+import AppLoading from 'expo-app-loading';
+import theme from './src/styles/theme';
+
+import { Home } from './src/screens/Home';
+import { ThemeProvider } from 'styled-components';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  )
+}
+
+
