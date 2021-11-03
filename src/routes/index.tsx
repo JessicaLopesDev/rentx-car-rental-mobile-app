24568f618f56e29fe6e31dce6d1fc3d1734676ba
JessicaLopesDev <jessicalopes.dev@gmail.com>
2021-11-03 19:30:24 -0300

    installing watermelondb

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppStackRoutes } from './app.stack.routes';
 import { useAuth } from '../hooks/auth';
 import { AppTabRoutes } from './app.tab.routes ';
 import { AuthRoutes } from './auth.routes';
